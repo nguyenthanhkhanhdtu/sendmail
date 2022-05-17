aca0b331e71b176b7e0d308417fa8e5bb42d5775
nguyenthanhkhanhdtu <51075284+nguyenthanhkhanhdtu@users.noreply.github.com>
2022-05-17 08:53:21 +0700

    Add files via upload
    Script write Node JS

---
+++ send-mail.js
@@ -0,0 +1,24 @@
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'anhhung123anhhung123@gmail.com',
+    pass: 'anhhung123'
+  }
+});
+
+var mailOptions = {
+  from: 'anhhung123anhhung123@gmail.com',
+  to: 'nguyenthanhkhanhdtu@gmail.com,boss.dx.kingkurri@gmail.com,minhthong.tao96@gmail.com',
+  subject: 'HAKER',
+  text: 'Xin chào! Tôi là hacker. Hãy đưa tiền cho tôi'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
